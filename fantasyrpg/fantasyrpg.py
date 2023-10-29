f50910b29911c6714696c523b40594d51b2d2560
larmen9179 <lo.j.armendariz@gmail.com>
2023-10-29 13:38:35 -0600

    added assets files and altered map printing

--- fantasyrpg/fantasyrpg.py
+++ fantasyrpg/fantasyrpg.py
@@ -1,16 +1,23 @@
+import climage
 
-map = [[1, 1, 1, 1, 1],
+for i in range(3):
+    print()
+
+map = [[0, 1, 0, 1, 0],
+       [1, 1, 0, 1, 1],
        [1, 0, 0, 0, 1],
        [1, 0, 1, 0, 1],
        [1, 0, 0, 0, 1],
        [1, 1, 1, 1, 1]]
 
+
+
 for i in range(len(map)):
     for j in range(len(map[i])):
         if map[i][j] == 1:
-            print("|****|", end="")
+            print("|*****|", end = "")
         else:
-            print("      ", end = "")
+            print("   -   ", end = "")
     
     print()
     print()
